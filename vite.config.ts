045eed670b98cf1819f1c197334ea89c2cc77609
汪明杰 <1209497940@qq.com>
2025-12-16 20:40:34 +0800

    update page url

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: "/2025-birthday/", 
+      base: "/oliver-tongji-2025/", 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
